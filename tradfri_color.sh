cc02d9985183ff21e5f9f5b017b1152a35d3d8e2
Johan Nilsson <jn@gobo.ws>
2017-04-26 20:36:22 +0200

    Delete tradfri_color.sh

--- tradfri_color.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# tradfri_color.sh - OLA Trigger config to control IKEA Trådfri lights
-# https://github.com/gobo-ws/ola-trigger-ikea-tradfri-dmx
-coap-client -m put -u "Client_identity" -k "$1" -e '{ "3311": [{ "5706": '$2' }] }' "coaps://$3:5684/15001/$4"
