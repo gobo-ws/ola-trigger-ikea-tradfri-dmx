edcb776eede754156b5f36275785c12f742d05e7
Johan Nilsson <jn@gobo.ws>
2017-04-26 20:38:04 +0200

    Update tradfri.sh

--- tradfri.sh
+++ tradfri.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
 # tradfri.sh - OLA Trigger config to control IKEA Trådfri lights
 # https://github.com/gobo-ws/ola-trigger-ikea-tradfri-dmx
-coap-client -m put -u "Client_identity" -k "$1" -e '{ "3311": [{ "5850": '$2' }] }' "coaps://$3:5684/15001/$4"
+
+if [ "$1" = "dimmer" ]
+then
+    coap-client -m put -k "$2" -e '{ "3311": [{ "5851": '$3' }] }' "coaps://$4:5684/15001/$5"
+elif [ "$1" = "color" ]
+then
+    coap-client -m put -k "$2" -e '{ "3311": [{ "5706": '$3' }] }' "coaps://$4:5684/15001/$5"
+fi
