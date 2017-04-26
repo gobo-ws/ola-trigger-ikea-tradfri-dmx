18f9742c60434bc2c7190fa75c20cf56811b023e
Johan Nilsson <jn@gobo.ws>
2017-04-26 20:36:31 +0200

    Delete tradfri_dimmer.sh

--- tradfri_dimmer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# tradfri_dimmer.sh - OLA Trigger config to control IKEA Trådfri lights
-# https://github.com/gobo-ws/ola-trigger-ikea-tradfri-dmx
-coap-client -m put -u "Client_identity" -k "$1" -e '{ "3311": [{ "5851": '$2' }] }' "coaps://$3:5684/15001/$4"
